ca68c79f4ba0ed1f4869b092ebb3bbabda4a7ebb
Tamara Ringas <tamriasdev@gmail.com>
2026-02-16 18:13:30 +0200

    feat: add events and errors

--- src/interfaces/ICurve.sol
+++ src/interfaces/ICurve.sol
@@ -1,1 +1,38 @@
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+/// @title ICurve
+/// @notice Interface for the bonding curve contract
+/// @author balanced-tree
+interface ICurve {
+    /*//////////////////////////////////////////////////////////////
+                                STRUCTS
+    //////////////////////////////////////////////////////////////*/
+    struct CurveConfig {
+      uint256 maxThreshold;
+      uint256 minThreshold;
+      uint256 timeoutPeriod;
+    }
+  
+    /*//////////////////////////////////////////////////////////////
+                                  EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event curveInitialized(uint256 maxThreshold, uint256 minThreshold, uint256 timeoutPeriod);
+    event collateralWithdrawn(address indexed user, uint256 indexed amount);
+  
+    /*//////////////////////////////////////////////////////////////
+                                  ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error NOT_INITIALIZED();
+  
+    error INVALID_TIMEOUT();
+  
+    error INVALID_THRESHOLD();
+  
+    error INVALID_AMOUNT();
+  
+    /*//////////////////////////////////////////////////////////////
+                                FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function initialize(CurveConfig calldata config) external;
+}
