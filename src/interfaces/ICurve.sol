4c032aa246f9765e339bfd103ca2870654383216
Tamara Ringas <tamriasdev@gmail.com>
2026-02-16 18:14:14 +0200

    chore: format

--- src/interfaces/ICurve.sol
+++ src/interfaces/ICurve.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-/// @title ICurve
+/// @title  ICurve
 /// @notice Interface for the bonding curve contract
 /// @author balanced-tree
 interface ICurve {
@@ -13,24 +13,24 @@ interface ICurve {
       uint256 minThreshold;
       uint256 timeoutPeriod;
     }
-  
+
     /*//////////////////////////////////////////////////////////////
                                   EVENTS
     //////////////////////////////////////////////////////////////*/
     event curveInitialized(uint256 maxThreshold, uint256 minThreshold, uint256 timeoutPeriod);
     event collateralWithdrawn(address indexed user, uint256 indexed amount);
-  
+
     /*//////////////////////////////////////////////////////////////
                                   ERRORS
     //////////////////////////////////////////////////////////////*/
     error NOT_INITIALIZED();
-  
+
     error INVALID_TIMEOUT();
-  
+
     error INVALID_THRESHOLD();
-  
+
     error INVALID_AMOUNT();
-  
+
     /*//////////////////////////////////////////////////////////////
                                 FUNCTIONS
     //////////////////////////////////////////////////////////////*/
