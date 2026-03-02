2d0fff46a9af069369661f62d433086e32b1b5f3
balanced-tree <tamriasdev@gmail.com>
2026-03-02 19:18:52 +0200

    feat: add factory interface

--- src/interfaces/ICurveFactory.sol
+++ src/interfaces/ICurveFactory.sol
@@ -1,57 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
+import { CreateCurveParams } from "../Types.sol";
+
+/// @title ICurveFactory
+/// @notice Interface for the bonding curve factory
 interface ICurveFactory {
-    /*//////////////////////////////////////////////////////////////
-                                 STATE
-    //////////////////////////////////////////////////////////////*/
-    struct CurveConfig {
-        address curveToken;
-        address collateralToken;
-        PiecewiseConfig[] segments;
-        VestingConfig vestingConfig;
-    }
-
-    struct PiecewiseConfig {
-        uint256 supplyStart;
-        uint256 supplyEnd;
-        FormulaType formulaType;
-        FormulaParams formulaParams;
-    }
-
-    struct FormulaParams {
-        int256[] params;
-    }
-
-    struct VestingConfig {
-        address vestingToken;
-        uint256 cliffDuration;
-        uint256 vestingDuration;
-    }
-
-    enum FormulaType {
-        LINEAR,
-        LN,
-        SIN,
-        PARABOLIC,
-        EXPONENTIAL,
-        SIGMOID
-    }
-
     /*//////////////////////////////////////////////////////////////
                                 EVENTS
     //////////////////////////////////////////////////////////////*/
-    event CurveCreated(address indexed curve, address indexed creator);
-    event VestingCreated(address indexed vesting, address indexed creator);
-    event GraduationManagerCreated(address indexed graduationManager, address indexed creator);
+    event CurveCreated(address indexed curve, address indexed token, address indexed creator);
 
     /*//////////////////////////////////////////////////////////////
                                 ERRORS
     //////////////////////////////////////////////////////////////*/
     error INVALID_ADDRESS();
     error INVALID_FEE_BPS();
+    error INVALID_SEGMENTS();
 
     /*//////////////////////////////////////////////////////////////
-                                FUNCTIONS
+                              FUNCTIONS
     //////////////////////////////////////////////////////////////*/
+    function createCurve(CreateCurveParams calldata params) external returns (address curve);
+    function getCurves() external view returns (address[] memory);
+    function getCurveCount() external view returns (uint256);
+    function isCurve(address curve) external view returns (bool);
 }
