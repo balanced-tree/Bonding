47c332b9a05ff0fc38648e4de1ec7f4069000257
balanced-tree <tamriasdev@gmail.com>
2026-03-04 15:29:55 +0200

    feat: add contract

--- src/contracts/Vesting.sol
+++ src/contracts/Vesting.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
+
+// OpenZeppelin Contracts
+import { Initializable } from "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import { ReentrancyGuardTransient } from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
+
+// Contracts
+import { IVesting } from "../interfaces/IVesting.sol";
+
+abstract contract Vesting is Initializable, IVesting {
+
+}
