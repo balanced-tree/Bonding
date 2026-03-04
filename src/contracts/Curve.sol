e45a641b7c506144646e413d76ea0cb44e9a66a0
balanced-tree <tamriasdev@gmail.com>
2026-03-04 09:22:31 +0200

    feat: setup inhreitance

--- src/contracts/Curve.sol
+++ src/contracts/Curve.sol
@@ -2,19 +2,19 @@
 pragma solidity 0.8.30;
 
 import { ICurve } from "../interfaces/ICurve.sol";
+import { IVesting } from "../interfaces/IVesting.sol";
 
 // OpenZeppelin Contracts
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { ReentrancyGuardTransient } from "@openzeppelin/contracts/utils/ReentrancyGuardTransient.sol";
 
 // OpenZeppelin Upgradeable
 import { ERC20Upgradeable } from "@openzeppelin-contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import { Initializable } from "@openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
-import { Ownable2StepUpgradeable } from "@openzeppelin-contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
-contract Curve is ICurve {
+abstract contract Curve is ICurve, Initializable, ReentrancyGuardTransient {
     using SafeERC20 for IERC20;
     using Math for uint256;
 
