93898396b20b94f0d35ff9c17d0b5ee442a9811f
balanced-tree <tamriasdev@gmail.com>
2026-03-09 16:06:33 +0200

    chore: natspec

--- src/contracts/Curve.sol
+++ src/contracts/Curve.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-
+// Contracts
 import { ICurve } from "../interfaces/ICurve.sol";
 import { BondingToken } from "./BondingToken.sol";
 import { PriceLib } from "../libraries/PriceLib.sol";
