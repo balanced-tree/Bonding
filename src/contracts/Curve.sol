483fdb041acbd20be68ad72781bcbdaec485fd0b
balanced-tree <tamriasdev@gmail.com>
2026-03-09 16:05:51 +0200

    fix: naming conventions

--- src/contracts/Curve.sol
+++ src/contracts/Curve.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
+
 import { ICurve } from "../interfaces/ICurve.sol";
 import { BondingToken } from "./BondingToken.sol";
 import { PriceLib } from "../libraries/PriceLib.sol";
