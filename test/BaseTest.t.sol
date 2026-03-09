5103f201c93244970b0513b5484c9c820e7858b8
balanced-tree <tamriasdev@gmail.com>
2026-03-09 17:17:41 +0200

    fix: imports

--- test/BaseTest.t.sol
+++ test/BaseTest.t.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-// Forge Std
-import { Test } from "forge-std/Test.sol";
-
 // Types
 import * as Types from "../src/Types.sol";
+
+// Forge Std
+import { Test } from "forge-std/Test.sol";
+import { console2 } from "forge-std/console2.sol";
 
 // Contracts
 import { Curve } from "../src/contracts/Curve.sol";
@@ -17,7 +18,6 @@ import { GraduationManager } from "../src/contracts/GraduationManager.sol";
 // Openzeppelin Contracts
 import { Clones } from "@openzeppelin/contracts/proxy/Clones.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 
 contract BaseTest is Test {
     function setUp() public virtual {
