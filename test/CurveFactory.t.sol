7510a57ec9049fed1f92836c13ed98651ce99020
balanced-tree <tamriasdev@gmail.com>
2026-03-10 12:19:36 +0200

    feat: add test contract

---
+++ test/CurveFactory.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import { BaseTest } from "./BaseTest.t.sol";
+
+contract CurveFactoryTest is BaseTest {
+    function setUp() public override {
+        super.setUp();
+    }
+}
