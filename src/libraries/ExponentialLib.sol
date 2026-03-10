938deae46abd179686bb52337854b6c15ee18647
balanced-tree <tamriasdev@gmail.com>
2026-03-10 12:17:10 +0200

    chore: format

--- src/libraries/ExponentialLib.sol
+++ src/libraries/ExponentialLib.sol
@@ -30,11 +30,7 @@ library ExponentialLib {
     /// @param sFrom Lower supply bound (SD59x18)
     /// @param sTo Upper supply bound (SD59x18)
     /// @return area The area under the price curve between sFrom and sTo
-    function integrate(
-        bytes memory encodedParams,
-        SD59x18 sFrom,
-        SD59x18 sTo
-    ) internal pure returns (SD59x18 area) {
+    function integrate(bytes memory encodedParams, SD59x18 sFrom, SD59x18 sTo) internal pure returns (SD59x18 area) {
         ExponentialParams memory p = abi.decode(encodedParams, (ExponentialParams));
         SD59x18 a = sd(p.a);
         SD59x18 k = sd(p.k);
--- src/libraries/LinearLib.sol
+++ src/libraries/LinearLib.sol
@@ -27,11 +27,7 @@ library LinearLib {
     /// @param sFrom Lower supply bound (SD59x18)
     /// @param sTo Upper supply bound (SD59x18)
     /// @return area The area under the price curve between sFrom and sTo
-    function integrate(
-        bytes memory encodedParams,
-        SD59x18 sFrom,
-        SD59x18 sTo
-    ) internal pure returns (SD59x18 area) {
+    function integrate(bytes memory encodedParams, SD59x18 sFrom, SD59x18 sTo) internal pure returns (SD59x18 area) {
         LinearParams memory p = abi.decode(encodedParams, (LinearParams));
         SD59x18 m = sd(p.m);
         SD59x18 b = sd(p.b);
