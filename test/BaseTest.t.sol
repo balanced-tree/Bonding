496299033710012b3112cd96c261fef8797ca8e0
balanced-tree <tamriasdev@gmail.com>
2026-03-11 08:42:56 +0200

    feat: set up tokens

--- test/BaseTest.t.sol
+++ test/BaseTest.t.sol
@@ -6,9 +6,9 @@ import "../src/Types.sol" as Types;
 
 // Forge Std
 import { Test } from "forge-std/Test.sol";
-import { console2 } from "forge-std/console2.sol";
 
 // Contracts
+import { MockERC20 } from "./MockERC20.sol";
 import { Curve } from "../src/contracts/Curve.sol";
 import { Vesting } from "../src/contracts/Vesting.sol";
 import { CurveFactory } from "../src/contracts/CurveFactory.sol";
@@ -17,8 +17,6 @@ import { GraduationManager } from "../src/contracts/GraduationManager.sol";
 
 // Openzeppelin Contracts
 import { Clones } from "@openzeppelin/contracts/proxy/Clones.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract BaseTest is Test {
     using Clones for address;
@@ -45,112 +43,46 @@ contract BaseTest is Test {
     // Protocol fee
     uint256 public protocolFeeBps;
 
+    // Tokens
+    MockERC20 public usdc;
+    MockERC20 public wbtc;
+
     function setUp() public virtual {
         protocolFeeBps = 1000; // 10%
 
-        // Set up fork chain config
-        _prepareForks();
-
-        // Deploy contract addresses
-        _deployContracts(protocolFeeBps);
+        // Create contract instances
+        _deployContracts();
 
         // Set up tokens
         _setTokens();
 
         // Set up test accounts
         _makeTestAccounts();
-        _fundAccounts(LARGE);
-
-        // Set up test contract instances
-        vm.selectFork(forks[ETH]);
-        curveFactory = CurveFactory(addresses[ETH].curveFactory);
-        curveImplementation = Curve(addresses[ETH].curveImplementation);
-        vestingImplementation = Vesting(addresses[ETH].vestingImplementation);
-        tokenImplementation = BondingToken(addresses[ETH].tokenImplementation);
-        graduationManagerImplementation = GraduationManager(addresses[ETH].graduationManagerImplementation);
-
-        protocolTreasury = addresses[ETH].protocolTreasury;
-        feeRecipient = addresses[ETH].feeRecipient;
+        _fundAccounts(1000);
     }
 
-    function _prepareForks() internal {
-        mapping(uint64 => uint256) storage forksPerChain = forks;
+    function _deployContracts() internal {
+        curveImplementation = new Curve();
+        vestingImplementation = new Vesting();
+        tokenImplementation = new BondingToken();
+        graduationManagerImplementation = new GraduationManager();
 
-        if (useLatestFork) {
-            forksPerChain[ETH] = vm.createFork(ethereumRpcUrl);
-            forksPerChain[OP] = vm.createFork(optimismRpcUrl);
-            forksPerChain[BASE] = vm.createFork(baseRpcUrl);
-        } else {
-            forksPerChain[ETH] = vm.createFork(ethereumRpcUrl, ETH_BLOCK);
-            forksPerChain[OP] = vm.createFork(optimismRpcUrl, OP_BLOCK);
-            forksPerChain[BASE] = vm.createFork(baseRpcUrl, BASE_BLOCK);
-        }
+        feeRecipient = makeAddr("feeRecipient");
+        protocolTreasury = makeAddr("protocolTreasury");
 
-        mapping(uint64 => string) storage rpc_urls = rpcURLs;
-        rpc_urls[ETH] = ethereumRpcUrl;
-        rpc_urls[OP] = optimismRpcUrl;
-        rpc_urls[BASE] = baseRpcUrl;
-    }
-
-    function _deployContracts(uint256 fee) internal {
-        for (uint64 i = 0; i < chainIds.length; i++) {
-            vm.selectFork(forks[chainIds[i]]);
-
-            // Deploy implementation contracts
-            address curveImpl = address(new Curve());
-            addresses[chainIds[i]].curveImplementation = curveImpl;
-            vm.label(addresses[chainIds[i]].curveImplementation, "CurveImplementation");
-
-            address vestingImpl = address(new Vesting());
-            addresses[chainIds[i]].vestingImplementation = vestingImpl;
-            vm.label(addresses[chainIds[i]].vestingImplementation, "VestingImplementation");
-
-            address tokenImpl = address(new BondingToken());
-            addresses[chainIds[i]].tokenImplementation = tokenImpl;
-            vm.label(addresses[chainIds[i]].tokenImplementation, "TokenImplementation");
-
-            address graduationManagerImpl = address(new GraduationManager());
-            addresses[chainIds[i]].graduationManagerImplementation = graduationManagerImpl;
-            vm.label(addresses[chainIds[i]].graduationManagerImplementation, "GraduationManagerImplementation");
-
-            address treasury = makeAddr("protocolTreasury");
-            addresses[chainIds[i]].protocolTreasury = treasury;
-            vm.label(addresses[chainIds[i]].protocolTreasury, "ProtocolTreasury");
-
-            address recipient = makeAddr("feeRecipient");
-            addresses[chainIds[i]].feeRecipient = recipient;
-            vm.label(addresses[chainIds[i]].feeRecipient, "FeeRecipient");
-
-            addresses[chainIds[i]].curveFactory = address(
-                new CurveFactory(
-                    address(curveImplementation),
-                    addresses[chainIds[i]].tokenImplementation,
-                    addresses[chainIds[i]].vestingImplementation,
-                    addresses[chainIds[i]].graduationManagerImplementation,
-                    addresses[chainIds[i]].protocolTreasury,
-                    fee
-                )
-            );
-            vm.label(addresses[chainIds[i]].curveFactory, "CurveFactory");
-        }
+        curveFactory = new CurveFactory(
+            address(curveImplementation),
+            address(tokenImplementation),
+            address(vestingImplementation),
+            address(graduationManagerImplementation),
+            address(protocolTreasury), 
+            protocolFeeBps
+        );
     }
 
     function _setTokens() internal {
-        // Mainnet tokens
-        tokens[ETH][WBTC_KEY] = CHAIN_1_WBTC;
-        tokens[ETH][DAI_KEY] = CHAIN_1_DAI;
-        tokens[ETH][USDC_KEY] = CHAIN_1_USDC;
-        tokens[ETH][WETH_KEY] = CHAIN_1_WETH;
-
-        // Optimism tokens
-        tokens[OP][DAI_KEY] = CHAIN_10_DAI;
-        tokens[OP][USDC_KEY] = CHAIN_10_USDC;
-        tokens[OP][WETH_KEY] = CHAIN_10_WETH;
-
-        // Base tokens
-        tokens[BASE][DAI_KEY] = CHAIN_8453_DAI;
-        tokens[BASE][USDC_KEY] = CHAIN_8453_USDC;
-        tokens[BASE][WETH_KEY] = CHAIN_8453_WETH;
+        usdc = new MockERC20("USDC", "USDC", 6);
+        wbtc = new MockERC20("WBTC", "WBTC", 18);
     }
 
     function _makeTestAccounts() internal {
@@ -172,17 +104,16 @@ contract BaseTest is Test {
     }
 
     function _fundAccounts(uint256 amount) internal {
-        for (uint256 i; i < tokenKeys.length; i++) {
-            for (uint256 j; j < chainIds.length; j++) {
-                address token = tokens[chainIds[j]][tokenKeys[i]];
-                if (token != address(0)) {
-                    uint256 decimals = IERC20Metadata(token).decimals();
-                    deal(token, curveCreator, amount * (10 ** decimals));
-                    deal(token, alice, amount * (10 ** decimals));
-                    deal(token, bob, amount * (10 ** decimals));
-                    deal(token, eve, amount * (10 ** decimals));
-                }
-            }
-        }
+        deal(address(usdc), curveCreator, amount * (10 ** 6));
+        deal(address(wbtc), curveCreator, amount * (10 ** 18));
+
+        deal(address(usdc), alice, amount * (10 ** 6));
+        deal(address(wbtc), alice, amount * (10 ** 18));
+
+        deal(address(usdc), bob, amount * (10 ** 6));
+        deal(address(wbtc), bob, amount * (10 ** 18));
+
+        deal(address(usdc), eve, amount * (10 ** 6));
+        deal(address(wbtc), eve, amount * (10 ** 18));
     }
 }
