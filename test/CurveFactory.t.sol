4c895b5fa073e7f433cf674fda480d53f0d0c95f
balanced-tree <tamriasdev@gmail.com>
2026-03-11 08:43:19 +0200

    feat: setUp() & test_createCurve()

--- test/CurveFactory.t.sol
+++ test/CurveFactory.t.sol
@@ -1,10 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
+// Types
+import "../src/Types.sol" as Types;
+
+import { Helpers } from "./Helpers.sol";
 import { BaseTest } from "./BaseTest.t.sol";
 
-contract CurveFactoryTest is BaseTest {
+contract CurveFactoryTest is BaseTest, Helpers {
     function setUp() public override {
         super.setUp();
+    }
+
+    function test_createCurve() public {
+        vm.startPrank(curveCreator);
+
+        Types.PiecewiseSegment[] memory segment = new Types.PiecewiseSegment[](1);
+        segment[0] = Types.PiecewiseSegment({
+            supplyStart: 0,
+            supplyEnd: 1e18,
+            formulaType: Types.FormulaType.LINEAR,
+            encodedParams: abi.encode(Types.LinearParams({
+                m: 1e18,
+                b: 0
+            }))
+        });
+
+        curveFactory.createCurve(
+          Types.CreateCurveParams({
+              name: "Test Curve",
+              symbol: "TEST",
+              decimals: 18,
+              feeRecipient: feeRecipient,
+              feeRecipientBps: protocolFeeBps,
+              curveParams: Types.CurveParams({
+                  collateralToken: address(usdc),
+                  segments: segment,
+                  maxThreshold: 1e18,
+                  maxBuyPerTx: 10_000,
+                  maxSellPerTx: 10_000
+              }),
+              vestingConfig: Types.VestingConfig({
+                  cliffDuration: 0,
+                  vestingDuration: 0
+              })
+        }));
     }
 }
