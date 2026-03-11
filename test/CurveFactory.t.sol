8355c52a6f6a718064e1f98d2895c5f58c5f7e11
balanced-tree <tamriasdev@gmail.com>
2026-03-11 08:54:44 +0200

    chore: format

--- test/CurveFactory.t.sol
+++ test/CurveFactory.t.sol
@@ -20,35 +20,31 @@ contract CurveFactoryTest is BaseTest, Helpers {
             supplyStart: 0,
             supplyEnd: 1e18,
             formulaType: Types.FormulaType.LINEAR,
-            encodedParams: abi.encode(Types.LinearParams({
-                m: 1e18,
-                b: 0
-            }))
+            encodedParams: abi.encode(Types.LinearParams({ m: 1e18, b: 0 }))
         });
 
         (address curve, address token, address vesting, address graduationManager) = curveFactory.createCurve(
-          Types.CreateCurveParams({
-              name: "Test Curve",
-              symbol: "TEST",
-              decimals: 18,
-              feeRecipient: feeRecipient,
-              feeRecipientBps: protocolFeeBps,
-              curveParams: Types.CurveParams({
-                  collateralToken: address(usdc),
-                  segments: segment,
-                  maxThreshold: 1e18,
-                  maxBuyPerTx: 10_000,
-                  maxSellPerTx: 10_000
-              }),
-              vestingConfig: Types.VestingConfig({
-                  cliffDuration: 0,
-                  vestingDuration: 0
-              })
-        }));
+            Types.CreateCurveParams({
+                name: "Test Curve",
+                symbol: "TEST",
+                decimals: 18,
+                feeRecipient: feeRecipient,
+                feeRecipientBps: protocolFeeBps,
+                curveParams: Types.CurveParams({
+                    collateralToken: address(usdc),
+                    segments: segment,
+                    maxThreshold: 1e18,
+                    maxBuyPerTx: 10_000,
+                    maxSellPerTx: 10_000
+                }),
+                vestingConfig: Types.VestingConfig({ cliffDuration: 0, vestingDuration: 0 })
+            })
+        );
 
         assertEq(curveFactory.getCurveCount(), 1);
         assertEq(curveFactory.isCurve(address(curve)), true);
         assertEq(curveFactory.getCurves()[0], address(curve));
+
         assertEq(curveFactory.getToken(address(curve)), address(token));
         assertEq(curveFactory.getVesting(address(curve)), address(0));
         assertEq(curveFactory.getGraduationManager(address(curve)), address(graduationManager));
