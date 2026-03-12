0e951f82e6360a129aa2ce423c428f9580041713
balanced-tree <tamriasdev@gmail.com>
2026-03-12 09:50:08 +0200

    feat: add contract

---
+++ test/Curve.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+// Types
+import "../src/Types.sol" as Types;
+
+// Contracts
+import { Curve } from "../src/contracts/Curve.sol";
+import { BondingToken } from "../src/contracts/BondingToken.sol";
+import { ICurveFactory } from "../src/interfaces/ICurveFactory.sol";
+
+import { Helpers } from "./Helpers.sol";
+import { BaseTest } from "./BaseTest.t.sol";
+
+contract CurveTest is BaseTest, Helpers {
+
+}
