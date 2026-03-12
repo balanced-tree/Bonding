5e7c3bc1e1ccd564a540ab701e4ef25fdbf5362c
balanced-tree <tamriasdev@gmail.com>
2026-03-12 11:41:12 +0200

    chore: format

--- test/Curve.t.sol
+++ test/Curve.t.sol
@@ -4,15 +4,15 @@ pragma solidity 0.8.30;
 // Types
 import "../src/Types.sol" as Types;
 
+import { Helpers } from "./Helpers.sol";
+import { BaseTest } from "./BaseTest.t.sol";
+
 // Contracts
 import { Curve } from "../src/contracts/Curve.sol";
 import { Vesting } from "../src/contracts/Vesting.sol";
 import { BondingToken } from "../src/contracts/BondingToken.sol";
 import { ICurveFactory } from "../src/interfaces/ICurveFactory.sol";
 import { GraduationManager } from "../src/contracts/GraduationManager.sol";
-
-import { Helpers } from "./Helpers.sol";
-import { BaseTest } from "./BaseTest.t.sol";
 
 contract CurveTest is BaseTest, Helpers {
     Curve public curve;
