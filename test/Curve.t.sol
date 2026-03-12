31984cd0a61ac9b2f2e33b33850e00fda5ae4969
balanced-tree <tamriasdev@gmail.com>
2026-03-12 13:45:25 +0200

    feat: add test_initialize()

--- test/Curve.t.sol
+++ test/Curve.t.sol
@@ -55,7 +55,52 @@ contract CurveTest is BaseTest, Helpers {
         graduationManager = GraduationManager(_gm);
     }
 
-    function test_initialize() public {
+    function test_initialize() public view {
+        // Token & collateral addresses
         assertEq(curve.token(), address(token));
+        assertEq(curve.getTokenAddress(), address(token));
+        assertEq(curve.collateralToken(), address(usdc));
+        assertEq(curve.getCollateralAddress(), address(usdc));
+
+        // Protocol addresses
+        assertEq(curve.treasury(), protocolTreasury);
+        assertEq(curve.graduationManager(), address(graduationManager));
+
+        // No vesting configured (both cliff and duration are 0)
+        assertEq(curve.vesting(), address(0));
+
+        // Fee configuration
+        assertEq(curve.protocolFeeBps(), protocolFeeBps);
+        assertEq(curve.feeRecipient(), feeRecipient);
+        assertEq(curve.feeRecipientBps(), protocolFeeBps);
+
+        // Curve parameters
+        assertEq(curve.maxThreshold(), 1e18);
+        assertEq(curve.maxBuyPerTx(), 10_000);
+        assertEq(curve.maxSellPerTx(), 10_000);
+
+        // Not graduated
+        assertFalse(curve.graduated());
+
+        // Two segments stored (LINEAR + PARABOLIC)
+        (uint256 s0Start, uint256 s0End, Types.FormulaType s0Type,) = curve.segments(0);
+        assertEq(s0Start, 0);
+        assertEq(s0End, 50_000e18);
+        assertEq(uint8(s0Type), uint8(Types.FormulaType.LINEAR));
+
+        (uint256 s1Start, uint256 s1End, Types.FormulaType s1Type,) = curve.segments(1);
+        assertEq(s1Start, 50_000e18);
+        assertEq(s1End, 100_000e18);
+        assertEq(uint8(s1Type), uint8(Types.FormulaType.PARABOLIC));
+
+        // Initial supply is 0 so spot price should reflect supply=0 on the linear segment
+        // With default linear params (m=1, b=0): p(0) = 0
+        assertEq(curve.getPrice(), 0);
+
+        // Token metadata
+        assertEq(token.name(), "Bonding Curve");
+        assertEq(token.symbol(), "BOND");
+        assertEq(token.decimals(), 18);
+        assertEq(token.totalSupply(), 0);
     }
 }
