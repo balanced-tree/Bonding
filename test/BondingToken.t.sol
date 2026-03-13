e16a191603460a3f81e4f2abe7b3123dac8ddab5
balanced-tree <tamriasdev@gmail.com>
2026-03-13 08:40:34 +0200

    feat: vars and setup

--- test/BondingToken.t.sol
+++ test/BondingToken.t.sol
@@ -10,11 +10,14 @@ import { BaseTest } from "./BaseTest.t.sol";
 
 // Contracts
 import { Curve } from "../src/contracts/Curve.sol";
-import { Vesting } from "../src/contracts/Vesting.sol";
 import { BondingToken } from "../src/contracts/BondingToken.sol";
-import { ICurveFactory } from "../src/interfaces/ICurveFactory.sol";
-import { GraduationManager } from "../src/contracts/GraduationManager.sol";
 
 contract BondingTokenTest is BaseTest, Helpers{
+    Curve public curve;
+    BondingToken public token;
 
+    function setUp() public override {
+        super.setUp();
+
+    }
 }
