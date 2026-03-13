fa25a376062c668be4a1c7289cec2c252e2019f3
balanced-tree <tamriasdev@gmail.com>
2026-03-13 08:59:55 +0200

    feat: add contract

---
+++ test/Integration.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+// Types
+import "../../src/Types.sol" as Types;
+
+// Testing
+import { BaseTest } from "../BaseTest.t.sol";
+import { Helpers } from "../utils/Helpers.sol";
+
+contract IntegrationTest is BaseTest, Helpers {
+
+}
