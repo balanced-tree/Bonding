6cbbee20c80009b2d3eff3084885ede3a16d729b
balanced-tree <tamriasdev@gmail.com>
2026-03-13 08:56:37 +0200

    feat: add contract

---
+++ test/libraries/LinearLib.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+// Types
+import "../../src/Types.sol" as Types;
+
+// Testing
+import { BaseTest } from "../BaseTest.t.sol";
+import { Helpers } from "../utils/Helpers.sol";
+
+// Libraries
+import { LinearLib } from "../../src/libraries/LinearLib.sol";
+
+contract LinearLibTest is BaseTest, Helpers {
+
+}
