a259293e218f13fc73cc535adfbe7431e737b22a
balanced-tree <tamriasdev@gmail.com>
2026-03-13 08:59:01 +0200

    feat: add contract

---
+++ test/libraries/PriceLib.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+// Types
+import "../../src/Types.sol" as Types;
+
+// Testing
+import { BaseTest } from "../BaseTest.t.sol";
+import { Helpers } from "../utils/Helpers.sol";
+
+// Libraries
+import { PriceLib } from "../../src/libraries/PriceLib.sol";
+
+contract PriceLibTest is BaseTest, Helpers {
+
+}
